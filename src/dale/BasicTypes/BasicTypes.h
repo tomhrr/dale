e5f1b481c2ea5e67bc76895505252aac06aca2d6
Tom Harrison <tomh5908@gmail.com>
2015-01-15 19:21:01 +1000

    [master] documentation

--- src/dale/BasicTypes/BasicTypes.h
+++ src/dale/BasicTypes/BasicTypes.h
@@ -16,9 +16,25 @@
 
 namespace dale
 {
+/*! BasicTypes
+
+    Provides a set of functions for instantiating the functions
+    required for core types, such as integers and floating-point
+    numbers.
+*/
 namespace BasicTypes
 {
-
+/*! Add a unary function to the context and module.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param name The name of the function.
+ *  @param return_type The return type for the new function.
+ *  @param type1 The type of the single parameter of the new function.
+ *
+ *  The caller has to add instructions to the body of the function, if
+ *  required.
+ */
 Element::Function *
 addSimpleUnaryFunction(Context *ctx,
                        llvm::Module *mod,
@@ -27,6 +43,18 @@ addSimpleUnaryFunction(Context *ctx,
                        Element::Type *return_type,
                        Element::Type *type1);
 
+/*! Add a binary function to the context and module.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param name The name of the function.
+ *  @param return_type The return type for the new function.
+ *  @param type1 The type of the first parameter of the new function.
+ *  @param type2 The type of the second parameter of the new function.
+ *
+ *  As with addSimpleUnaryFunction, the caller has to add instructions
+ *  to the body of the function, if required.
+ */
 Element::Function *
 addSimpleBinaryFunction(Context *ctx,
                         llvm::Module *mod,
@@ -36,6 +64,15 @@ addSimpleBinaryFunction(Context *ctx,
                         Element::Type *type1,
                         Element::Type *type2);
 
+/*! Add a binary function, mapping to an IRBuilder instruction.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param name The name of the function.
+ *  @param method_name An IRBuilder method pointer.
+ *  @param return_type The return type for the new function.
+ *  @param type The type of the two parameters of the new function.
+ */
 void
 makeFunction(Context *ctx,
              llvm::Module *mod,
@@ -43,9 +80,19 @@ makeFunction(Context *ctx,
              const char *name,
              llvm::Value* (llvm::IRBuilder<>:: *method_name)
                 (llvm::Value*, llvm::Value*, const llvm::Twine &),
-             Element::Type *ret_type,
+             Element::Type *return_type,
              Element::Type *type);
 
+/*! Add a binary function for an enum type, mapping to an IRBuilder instruction.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param name The name of the function.
+ *  @param method_name An IRBuilder method pointer.
+ *  @param return_type The return type for the new function.
+ *  @param type The type (an enum) of the two parameters of the new function.
+ *  @param linkage The linkage for the new function.
+ */
 void
 makeEnumFunction(Context *ctx,
                  llvm::Module *mod,
@@ -55,26 +102,61 @@ makeEnumFunction(Context *ctx,
                      (llvm::Value*, llvm::Value*, const llvm::Twine &),
                  Element::Type *ret_type,
                  Element::Type *type,
-                 int mylinkage);
+                 int linkage);
 
+/*! Instantiate the functions required for the given signed integer type.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param type The signed integer type.
+ *
+ *  At the moment, these functions are +, -, /, *, =, !=, <, <=, >,
+ *  >=, <<, >>, &, | and ^.
+ */
 void
 addSignedInt(Context *ctx,
              llvm::Module *mod,
              std::string *once_tag,
              Element::Type *type);
 
+/*! Instantiate the functions required for the given floating point type.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param type The floating point type.
+ *
+ *  At the moment, these functions are +, -, /, *, =, !=, <, <=, > and
+ *  >=.
+ */
 void
 addFloatingPoint(Context *ctx,
                  llvm::Module *mod,
                  std::string *once_tag,
                  Element::Type *type);
 
+/*! Instantiate the functions required for the given unsigned integer type.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param type The unsigned integer type.
+ *
+ *  The functions defined are the same as those for signed integers,
+ *  except that complement (~) is also defined over unsigned integers.
+ */
 void
 addUnsignedInt(Context *ctx,
                llvm::Module *mod,
                std::string *once_tag,
                Element::Type *type);
 
+/*! Instantiate the functions required for the given enum type.
+ *  @param ctx The context.
+ *  @param mod The LLVM module.
+ *  @param once_tag The current once tag, if applicable.
+ *  @param type The enum type.
+ *
+ *  The functions defined are the same as those for signed integers.
+ */
 void
 addEnum(Context *ctx,
         llvm::Module *mod,
@@ -83,7 +165,6 @@ addEnum(Context *ctx,
         Element::Type *enum_int_type,
         llvm::Type *llvm_enum_int_type,
         int flinkage);
-
 }
 }
 
