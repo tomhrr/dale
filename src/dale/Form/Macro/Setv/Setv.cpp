627b5f088d84257d3aacd7e22dfe9c08f89a5746
Tom Harrison <tomh5908@gmail.com>
2015-03-03 19:13:52 +1000

    [master] tidying

--- src/dale/Form/Macro/Setv/Setv.cpp
+++ src/dale/Form/Macro/Setv/Setv.cpp
@@ -4,35 +4,24 @@ namespace dale {
 Node *
 FormMacroSetvParse(Context *ctx, Node *n)
 {
-    assert(n->list && "parseSetv must receive a list!");
-
     if (!ctx->er->assertArgNums("setv", n, 2, 2)) {
         return NULL;
     }
 
     std::vector<Node*> *lst = n->list;
-    Node *varn = (*lst)[1];
 
-    if (!ctx->er->assertArgIsAtom("setv", varn, "1")) {
+    if (!ctx->er->assertArgIsAtom("setv", (*lst)[1], "1")) {
         return NULL;
     }
 
-    // (setv a 10) => (setf (# a) 10)
-
-    // Change the first token.
     (*lst)[0]->token->str_value.clear();
     (*lst)[0]->token->str_value.append("setf");
 
-    // Create a new nodelist, first element #, second element
-    // whatever the current second element is, make it the second
-    // element of the current list.
+    std::vector<Node*> *new_lst = new std::vector<Node *>;
+    new_lst->push_back(new Node("#"));
+    new_lst->push_back((*lst)[1]);
 
-    std::vector<Node*> *newlst = new std::vector<Node *>;
-    Token *addrof = new Token(TokenType::String);
-    addrof->str_value.append("#");
-    newlst->push_back(new Node(addrof));
-    newlst->push_back((*lst)[1]);
-    (*lst)[1] = new Node(newlst);
+    (*lst)[1] = new Node(new_lst);
 
     return n;
 }
--- src/dale/Form/Macro/Setv/Setv.h
+++ src/dale/Form/Macro/Setv/Setv.h
@@ -5,8 +5,13 @@
 #include "../../../Context/Context.h"
 
 namespace dale {
-Node *
-FormMacroSetvParse(Context *ctx, Node *node);
+/*! The setv core macro form.
+ *  @param ctx The context.
+ *  @param node The node list.
+ *
+ *  Expands (setv value new-value) to (setf (# value) new-value).
+ */
+Node *FormMacroSetvParse(Context *ctx, Node *node);
 }
 
 #endif
