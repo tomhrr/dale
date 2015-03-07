b11e2c31d081b5978dd6587e599ae73b9870ec3e
Tom Harrison <tomh5908@gmail.com>
2015-03-07 14:48:23 +1000

    [master] tidying

--- src/dale/Form/Proc/Setf/Setf.cpp
+++ src/dale/Form/Proc/Setf/Setf.cpp
@@ -7,67 +7,127 @@
 #include "../../Utils/Utils.h"
 #include "../../../llvm_Function.h"
 
+using namespace dale::ErrorInst::Generator;
+
 namespace dale
 {
 bool
-FormProcSetfParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr)
+destructTwo(Context *ctx, ParseResult *one, ParseResult *two,
+            ParseResult *three, llvm::IRBuilder<> *builder)
+{
+    one->block = two->block;
+    bool res = Operation::Destruct(ctx, one, three, builder);
+    if (!res) {
+        return false;
+    }
+    two->block = three->block;
+    res = Operation::Destruct(ctx, two, three, builder);
+    if (!res) {
+        return false;
+    }
+    return true;
+}
+
+Function *
+getSetfAssign(Context *ctx, Type *dst_type, Type *src_type)
+{
+    std::vector<Type *> types;
+    types.push_back(dst_type);
+    types.push_back(src_type);
+    return ctx->getFunction("setf-assign", &types, NULL, 0);
+}
+
+bool
+processSetfPtrCall(Context *ctx, Function *over_setf,
+                   llvm::IRBuilder<> *builder,
+                   ParseResult *pr_variable, ParseResult *pr_value,
+                   ParseResult *pr)
+{
+    llvm::Value *src_ptr =
+        llvm::cast<llvm::Value>(
+            builder->CreateAlloca(
+                ctx->toLLVMType(pr_value->type, NULL, false, false)
+            )
+        );
+    builder->CreateStore(pr_value->value, src_ptr);
+
+    std::vector<llvm::Value *> call_args;
+    call_args.push_back(pr_variable->value);
+    call_args.push_back(src_ptr);
+    llvm::Value *ret =
+        builder->CreateCall(over_setf->llvm_function,
+                            llvm::ArrayRef<llvm::Value*>(call_args));
+
+    ParseResult pr_destruct;
+    bool res = destructTwo(ctx, pr_variable, pr_value, &pr_destruct, builder);
+    if (!res) {
+        return false;
+    }
+
+    pr->set(pr_destruct.block, ctx->tr->type_bool, ret);
+    return true;
+}
+
+bool
+processSetfExactCall(Context *ctx, Function *over_setf,
+                     llvm::IRBuilder<> *builder,
+                     ParseResult *pr_variable, ParseResult *pr_value,
+                     ParseResult *pr)
+{
+    std::vector<llvm::Value *> call_args;
+    call_args.push_back(pr_variable->value);
+    call_args.push_back(pr_value->value);
+    llvm::Value *ret =
+        builder->CreateCall(over_setf->llvm_function,
+                            llvm::ArrayRef<llvm::Value*>(call_args));
+
+    ParseResult pr_destruct;
+    bool res = destructTwo(ctx, pr_variable, pr_value, &pr_destruct, builder);
+    if (!res) {
+        return false;
+    }
+
+    pr->set(pr_destruct.block, ctx->tr->type_bool, ret);
+    return true;
+}
+
+bool
+FormProcSetfParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                  Node *node, bool get_address, bool prefixed_with_core,
+                  ParseResult *pr)
 {
     Context *ctx = units->top()->ctx;
 
-    assert(node->list && "parseSetf must receive a list!");
-
-    symlist *lst = node->list;
+    std::vector<Node *> *lst = node->list;
 
     if (!ctx->er->assertArgNums("setf", node, 2, 2)) {
         return false;
     }
 
-    /* Used to use get_address for the first argument, but now setf
-     * always takes a pointer as its first argument, to facilitate
-     * overloading etc. */
-
     ParseResult pr_variable;
     bool res =
-        FormProcInstParse(units, fn, block, (*lst)[1], false, 
-                                    false, NULL,
-                                    &pr_variable);
-
+        FormProcInstParse(units, fn, block, (*lst)[1], false,
+                          false, NULL, &pr_variable);
     if (!res) {
         return false;
     }
 
-    /* Make sure that the first argument is a pointer. */
-
     if (!pr_variable.type->points_to) {
-        Error *e = new Error(
-            ErrorInst::Generator::IncorrectArgType,
-            (*lst)[1],
-            "setf", "a pointer", "1", "a value"
-        );
+        Error *e = new Error(IncorrectArgType, (*lst)[1],
+                             "setf", "a pointer", "1", "a value");
         ctx->er->addError(e);
         return false;
     }
-
-    /* Can't modify const variables. */
 
     if (pr_variable.type->points_to->is_const) {
-        Error *e = new Error(
-            ErrorInst::Generator::CannotModifyConstVariable,
-            node
-        );
+        Error *e = new Error(CannotModifyConstVariable, node);
         ctx->er->addError(e);
         return false;
     }
 
-    Node *val_node = (*lst)[2];
-    val_node = units->top()->mp->parsePotentialMacroCall(val_node);
-    if (!val_node) {
+    Node *value_node = (*lst)[2];
+    value_node = units->top()->mp->parsePotentialMacroCall(value_node);
+    if (!value_node) {
         return false;
     }
 
@@ -75,13 +135,8 @@ FormProcSetfParse(Units *units,
     ParseResult pr_value;
     pr_value.retval = pr_variable.value;
     pr_value.retval_type = pr_variable.type;
-    Variable *var_value = NULL;
 
-    /* If the value is a variable, or a variable dereference, load the
-     * underlying value directly, to shortcut setf-copy for types that
-     * define it. */
-
-    res = FormProcessValue(units, fn, block, val_node, get_address,
+    res = FormProcessValue(units, fn, block, value_node, get_address,
                            pr_variable.type->points_to, &pr_value);
     if (!res) {
         return false;
@@ -94,138 +149,56 @@ FormProcSetfParse(Units *units,
 
     builder.SetInsertPoint(pr_value.block);
 
-    /* If overridden setf exists, and pr_value is a value of the
-     * pointee type of pr_variable, then call overridden setf
-     * after allocating memory for pr_value and copying it into
-     * place. */
+    /* If an overridden setf exists, and pr_value is a value of the
+     * pointee type of pr_variable, then call the overridden setf
+     * after allocating memory for pr_value and copying it into place.
+     * */
 
     if (!prefixed_with_core
             && pr_variable.type->points_to->canBeSetFrom(pr_value.type)) {
-        std::vector<Type *> types;
-        types.push_back(pr_variable.type);
-        types.push_back(pr_variable.type);
-        Function *over_setf =
-            ctx->getFunction("setf-assign", &types, NULL, 0);
-        if (!over_setf) {
-            goto cont1;
+        Function *over_setf = getSetfAssign(ctx, pr_variable.type,
+                                            pr_variable.type);
+        if (over_setf) {
+            return processSetfPtrCall(ctx, over_setf, &builder,
+                                      &pr_variable, &pr_value, pr);
         }
-        llvm::Value *new_ptr2 = llvm::cast<llvm::Value>(
-                                    builder.CreateAlloca(
-                                        ctx->toLLVMType(pr_value.type, 
-                                                        NULL, false,
-                                                        false)
-                                    )
-                                );
-        builder.CreateStore(pr_value.value, new_ptr2);
-        std::vector<llvm::Value *> call_args;
-        call_args.push_back(pr_variable.value);
-        call_args.push_back(new_ptr2);
-        llvm::Value *ret =
-            builder.CreateCall(over_setf->llvm_function,
-                               llvm::ArrayRef<llvm::Value*>(call_args));
-
-        ParseResult temp;
-        pr_variable.block = pr_value.block;
-        bool mres = Operation::Destruct(ctx, &pr_variable, &temp, &builder);
-        if (!mres) {
-            return false;
-        }
-        pr_value.block = temp.block;
-        mres = Operation::Destruct(ctx, &pr_value, &temp, &builder);
-        if (!mres) {
-            return false;
-        }
-        pr->set(temp.block, ctx->tr->type_bool, ret);
-        return true;
     }
-
-cont1:
 
     /* If an appropriate setf definition exists, which matches
      * the arguments exactly, then use it. */
 
     if (!prefixed_with_core) {
-        std::vector<Type *> types;
-        types.push_back(pr_variable.type);
-        types.push_back(pr_value.type);
-        Function *over_setf =
-            ctx->getFunction("setf-assign", &types, NULL, 0);
-        if (!over_setf) {
-            goto cont2;
-        }
-        std::vector<llvm::Value *> call_args;
-        call_args.push_back(pr_variable.value);
-        call_args.push_back(pr_value.value);
-        llvm::Value *ret =
-            builder.CreateCall(over_setf->llvm_function,
-                               llvm::ArrayRef<llvm::Value*>(call_args));
-
-        ParseResult temp;
-        pr_variable.block = pr_value.block;
-        bool mres = Operation::Destruct(ctx, &pr_variable, &temp, &builder);
-        if (!mres) {
-            return false;
-        }
-        pr_value.block = temp.block;
-        mres = Operation::Destruct(ctx, &pr_value, &temp, &builder);
-        if (!mres) {
-            return false;
-        }
-
-        pr->set(temp.block, ctx->tr->type_bool, ret);
-        return true;
-    }
-
-cont2:
-
-    /* var_value is only present to support the overridden set
-     * operations: if this point is reached, then fall back to the
-     * standard form-processing logic. */
-
-    if (var_value) {
-        res =
-            FormProcInstParse(units, 
-                fn, pr_value.block, val_node, false,
-                false,
-                pr_variable.type->points_to,
-                &pr_value
-            );
-
-        if (!res) {
-            return false;
+        Function *over_setf = getSetfAssign(ctx, pr_variable.type,
+                                            pr_value.type);
+        if (over_setf) {
+            return processSetfExactCall(ctx, over_setf, &builder,
+                                        &pr_variable, &pr_value, pr);
         }
     }
 
     if (pr_variable.type->points_to->canBeSetFrom(pr_value.type)) {
         builder.CreateStore(pr_value.value, pr_variable.value);
 
-        ParseResult temp;
-        pr_variable.block = pr_value.block;
-        bool mres = Operation::Destruct(ctx, &pr_variable, &temp, &builder);
-        if (!mres) {
-            return false;
-        }
-        pr_value.block = temp.block;
-        mres = Operation::Destruct(ctx, &pr_value, &temp, &builder);
-        if (!mres) {
+        ParseResult pr_destruct;
+        bool res = destructTwo(ctx, &pr_variable, &pr_value,
+                               &pr_destruct, &builder);
+        if (!res) {
             return false;
         }
 
-        pr->set(temp.block, ctx->tr->type_bool, 
+        pr->set(pr_destruct.block, ctx->tr->type_bool,
                 llvm::ConstantInt::get(
                     llvm::IntegerType::get(llvm::getGlobalContext(), 1), 1
                 ));
-        pr->do_not_copy_with_setf = 1;
+        pr->do_not_copy_with_setf = true;
         return true;
     }
 
-    /* todo: it would be good to also show the setf-assign
-     * candidates here, if applicable. */
+    /* This is used to set an error message. */
     ctx->er->assertTypeEquality("setf", (*lst)[2],
-                            pr_value.type,
-                            pr_variable.type->points_to,
-                            false);
-
+                                pr_value.type,
+                                pr_variable.type->points_to,
+                                false);
     return false;
 }
 }
--- src/dale/Form/Proc/Setf/Setf.h
+++ src/dale/Form/Proc/Setf/Setf.h
@@ -3,14 +3,19 @@
 
 namespace dale
 {
+/*! Parse a procedure-body setf statement.
+ *  @param units The units context.
+ *  @param fn The function currently in scope.
+ *  @param block The current block.
+ *  @param node The node being parsed.
+ *  @param get_address Whether to return the address of the result.
+ *  @param prefixed_with_core Whether to ignore overridden setfs.
+ *  @param pr The parse result for the response.
+ */
 bool
-FormProcSetfParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr);
+FormProcSetfParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                  Node *node, bool get_address, bool prefixed_with_core,
+                  ParseResult *pr);
 }
 
 #endif
