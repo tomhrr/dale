88a8048ca93ab65541827b7a5ad221ca2f703f9c
Tom Harrison <tomh5908@gmail.com>
2015-03-09 21:30:35 +1000

    [master] tidying

--- src/dale/Form/Proc/PtrSubtract/PtrSubtract.h
+++ src/dale/Form/Proc/PtrSubtract/PtrSubtract.h
@@ -3,14 +3,19 @@
 
 namespace dale
 {
-bool
-FormProcPtrSubtractParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr);
+/*! Parse a pointer subtraction form.
+ *  @param units The units context.
+ *  @param fn The function currently in scope.
+ *  @param block The current block.
+ *  @param node The node being parsed.
+ *  @param get_address Whether to return the address of the result.
+ *  @param prefixed_with_core (Unused by this form.)
+ *  @param pr The parse result for the response.
+ */
+bool FormProcPtrSubtractParse(Units *units, Function *fn,
+                              llvm::BasicBlock *block, Node *node,
+                              bool get_address, bool prefixed_with_core,
+                              ParseResult *pr);
 }
 
 #endif
