33f145b4ed5828ac06e6787911aaed9891d86828
Tom Harrison <tomh5908@gmail.com>
2015-03-11 18:07:47 +1000

    [master] tidying

--- src/dale/Form/Proc/Null/Null.cpp
+++ src/dale/Form/Proc/Null/Null.cpp
@@ -6,46 +6,39 @@
 #include "../Inst/Inst.h"
 #include "../../../llvm_Function.h"
 
+using namespace dale::ErrorInst::Generator;
+
 namespace dale
 {
 bool
-FormProcNullParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr)
+FormProcNullParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                  Node *node, bool get_address, bool prefixed_with_core,
+                  ParseResult *pr)
 {
     Context *ctx = units->top()->ctx;
-
-    assert(node->list && "must receive a list!");
 
     if (!ctx->er->assertArgNums("null", node, 1, 1)) {
         return false;
     }
 
-    symlist *lst = node->list;
+    std::vector<Node *> *lst = node->list;
+    Node *node_value = (*lst)[1];
 
-    /* Take the second value, parsePotentialMacroCall, see if it's a
-     * list, if it is a list and the form is : or $, then show an
-     * error about that value never being null. */
+    /* Take the second value, call parsePotentialMacroCall, and see if
+     * it's a list.  If it is a list, and the form is : or $, then
+     * show an error about that value never being null. */
 
-    Node *arg = (*lst)[1];
-    if (arg->is_list) {
-        arg = units->top()->mp->parsePotentialMacroCall(arg);
-        if (!arg) {
+    if (node_value->is_list) {
+        node_value = units->top()->mp->parsePotentialMacroCall(node_value);
+        if (!node_value) {
             return false;
         }
-        if (arg->is_list) {
-            Node *first = arg->list->at(0);
+        if (node_value->is_list) {
+            Node *first = node_value->list->at(0);
             if (first->is_token) {
                 const char *v = first->token->str_value.c_str();
                 if ((!strcmp(v, ":")) || (!strcmp(v, "$"))) {
-                    Error *e = new Error(
-                        ErrorInst::Generator::ValueWillNeverBeNull,
-                        arg
-                    );
+                    Error *e = new Error(ValueWillNeverBeNull, node_value);
                     ctx->er->addError(e);
                     return false;
                 }
@@ -54,33 +47,32 @@ FormProcNullParse(Units *units,
     }
 
     ParseResult pr_value;
-    bool res = 
-        FormProcInstParse(units, fn, block, arg, false, 
-                                    false, NULL, &pr_value);
+    bool res = FormProcInstParse(units, fn, block, node_value, false,
+                                 false, NULL, &pr_value);
 
     if (!res) {
         return false;
     }
-    if (!ctx->er->assertIsPointerType("null", arg, pr_value.type, "1")) {
+    if (!ctx->er->assertIsPointerType("null", node_value, pr_value.type, "1")) {
         return false;
     }
 
     llvm::IRBuilder<> builder(pr_value.block);
-    llvm::Value *vres =
+    llvm::Value *int_res =
         builder.CreatePtrToInt(pr_value.value, ctx->nt->getNativeIntType());
 
-    llvm::Value *icmpres = llvm::cast<llvm::Value>(
-                               builder.CreateICmpEQ(vres,
-                                       ctx->nt->getLLVMZero())
-                           );
+    llvm::Value *null_res =
+        llvm::cast<llvm::Value>(
+            builder.CreateICmpEQ(int_res, ctx->nt->getLLVMZero())
+        );
 
-    pr->set(pr_value.block, ctx->tr->type_bool, icmpres);
-    ParseResult temp;
-    res = Operation::Destruct(ctx, &pr_value, &temp);
+    ParseResult pr_destruct;
+    res = Operation::Destruct(ctx, &pr_value, &pr_destruct);
     if (!res) {
         return false;
     }
-    pr->block = temp.block;
+
+    pr->set(pr_destruct.block, ctx->tr->type_bool, null_res);
 
     return true;
 }
--- src/dale/Form/Proc/Null/Null.h
+++ src/dale/Form/Proc/Null/Null.h
@@ -3,14 +3,18 @@
 
 namespace dale
 {
-bool
-FormProcNullParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr);
+/*! Parse a null pointer check form.
+ *  @param units The units context.
+ *  @param fn The function currently in scope.
+ *  @param block The current block.
+ *  @param node The node being parsed.
+ *  @param get_address Whether to return the address of the result.
+ *  @param prefixed_with_core (Unused by this form.)
+ *  @param pr The parse result for the response.
+ */
+bool FormProcNullParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                       Node *node, bool get_address, bool prefixed_with_core,
+                       ParseResult *pr);
 }
 
 #endif
