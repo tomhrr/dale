0dd74e8207006dacd6dfea560469002e521d786c
Tom Harrison <tomh5908@gmail.com>
2015-03-12 18:58:44 +1000

    [master] tidying

--- src/dale/Form/Proc/Funcall/Funcall.cpp
+++ src/dale/Form/Proc/Funcall/Funcall.cpp
@@ -5,59 +5,41 @@
 #include "../Inst/Inst.h"
 #include "../../../llvm_Function.h"
 
+using namespace dale::ErrorInst::Generator;
+
 namespace dale
 {
 bool
-FormProcFuncallParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr)
+FormProcFuncallParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                     Node *node, bool get_address, bool prefixed_with_core,
+                     ParseResult *pr)
 {
     Context *ctx = units->top()->ctx;
-
-    assert(node->list && "must receive a list!");
-
-    /* (funcall <pointer-to-function> <arg1> <arg2> ...) */
 
     if (!ctx->er->assertArgNums("funcall", node, 1, -1)) {
         return false;
     }
 
     std::vector<Node *> *lst = node->list;
+    Node *fp_node = (*lst)[1];
 
-    ParseResult fn_ptr;
-    bool res =
-        FormProcInstParse(units, 
-            fn, block, (*lst)[1], get_address, false, NULL, &fn_ptr
-        );
+    ParseResult pr_fp;
+    bool res = FormProcInstParse(units, fn, block, fp_node, get_address,
+                                 false, NULL, &pr_fp);
     if (!res) {
         return false;
     }
 
-    if (!fn_ptr.type->points_to
-            || !fn_ptr.type->points_to->is_function) {
-        std::string temp;
-        fn_ptr.type->toString(&temp);
-        Error *e = new Error(
-            ErrorInst::Generator::IncorrectArgType,
-            ((*lst)[1]),
-            "funcall", "fn pointer", "1", temp.c_str()
-        );
+    if (!pr_fp.type->points_to || !pr_fp.type->points_to->is_function) {
+        std::string type_str;
+        pr_fp.type->toString(&type_str);
+        Error *e = new Error(IncorrectArgType, fp_node, "funcall",
+                             "function pointer", "1", type_str.c_str());
         ctx->er->addError(e);
         return false;
     }
 
-    res = units->top()->fp->parseFunctionPointerCall(
-               fn,
-               node,
-               &fn_ptr,
-               2,
-               NULL,
-               pr
-           );
-    return res;
+    return units->top()->fp->parseFunctionPointerCall(fn, node, &pr_fp,
+                                                      2, NULL, pr);
 }
 }
