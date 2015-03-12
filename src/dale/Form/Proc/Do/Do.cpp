dda12c27300c40764b6ed935e3ed039f62ed811f
Tom Harrison <tomh5908@gmail.com>
2015-03-12 18:44:30 +1000

    [master] tidying

--- src/dale/Form/Proc/Do/Do.cpp
+++ src/dale/Form/Proc/Do/Do.cpp
@@ -9,50 +9,37 @@
 namespace dale
 {
 bool
-FormProcDoParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr)
+FormProcDoParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                Node *node, bool get_address, bool prefixed_with_core,
+                ParseResult *pr)
 {
     Context *ctx = units->top()->ctx;
-    assert(node->list && "must receive a list!");
+
     if (!ctx->er->assertArgNums("do", node, 1, -1)) {
         return false;
     }
 
-    pr->block = block;
-
     for (std::vector<Node*>::iterator b = (node->list->begin() + 1),
-                                      e = node->list->end();
+                                      e = (node->list->end()   - 1);
             b != e;
             ++b) {
-        bool is_last = ((b + 1) == e);
-
-        ParseResult local_pr;
-        ParseResult *used_pr = (is_last ? pr : &local_pr);
-        bool res = FormProcInstParse(units, 
-                       fn, pr->block, *b, get_address, 
-                       false, NULL,
-                       used_pr
-                   );
+        ParseResult pr_local;
+        bool res = FormProcInstParse(units, fn, block, *b, get_address,
+                                     false, NULL, &pr_local);
         if (!res) {
             return false;
         }
 
-        pr->block = used_pr->block;
-        if (!is_last) {
-            ParseResult pr_value;
-            bool res = Operation::Destruct(ctx, &local_pr, &pr_value);
-            if (!res) {
-                return false;
-            }
-            pr->block = pr_value.block;
+        ParseResult pr_destruct;
+        res = Operation::Destruct(ctx, &pr_local, &pr_destruct);
+        if (!res) {
+            return false;
         }
+        block = pr_destruct.block;
     }
 
-    return true;
+    return FormProcInstParse(units, fn, block,
+                             *(node->list->end() - 1),
+                             get_address, false, NULL, pr);
 }
 }
