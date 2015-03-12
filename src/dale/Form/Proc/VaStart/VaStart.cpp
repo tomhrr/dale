5544343a8d0b14191055d267c72a5897dbbde18a
Tom Harrison <tomh5908@gmail.com>
2015-03-12 18:11:14 +1000

    [master] tidying

--- src/dale/Form/Proc/VaStart/VaStart.cpp
+++ src/dale/Form/Proc/VaStart/VaStart.cpp
@@ -9,13 +9,9 @@
 namespace dale
 {
 bool
-FormProcVaStartParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr)
+FormProcVaStartParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                     Node *node, bool get_address, bool prefixed_with_core,
+                     ParseResult *pr)
 {
     Context *ctx = units->top()->ctx;
 
@@ -23,13 +19,11 @@ FormProcVaStartParse(Units *units,
         return false;
     }
 
-    symlist *lst = node->list;
+    std::vector<Node *> *lst = node->list;
 
     ParseResult pr_valist;
-    bool res =
-        FormProcInstParse(units, fn, block, (*lst)[1], false,
-                                false, NULL,
-                               &pr_valist);
+    bool res = FormProcInstParse(units, fn, block, (*lst)[1], false,
+                                 false, NULL, &pr_valist);
     if (!res) {
         return false;
     }
@@ -37,26 +31,20 @@ FormProcVaStartParse(Units *units,
     llvm::IRBuilder<> builder(pr_valist.block);
     llvm::Function *va_start =
         units->top()->module->getFunction(llvm::StringRef("llvm.va_start"));
-    if (!va_start) {
-        fprintf(stderr, "Unable to load va_start.");
-        abort();
-    }
 
-    std::vector<llvm::Value*> call_args;
-    ParseResult to_pchar;
-    res = Operation::Cast(ctx, pr_valist.block,
-                pr_valist.value,
-                pr_valist.type,
-                ctx->tr->getPointerType(ctx->tr->type_char),
-                node, 0, &to_pchar);
+    ParseResult pr_pchar;
+    res = Operation::Cast(ctx, pr_valist.block, pr_valist.value,
+                          pr_valist.type, ctx->tr->type_pchar,
+                          node, 0, &pr_pchar);
     if (!res) {
         return false;
     }
-    call_args.push_back(to_pchar.value);
-    builder.CreateCall(va_start,
-                       llvm::ArrayRef<llvm::Value*>(call_args));
 
-    pr->set(to_pchar.block, ctx->tr->type_void, NULL);
+    std::vector<llvm::Value*> call_args;
+    call_args.push_back(pr_pchar.value);
+    builder.CreateCall(va_start, llvm::ArrayRef<llvm::Value*>(call_args));
+
+    pr->set(pr_pchar.block, ctx->tr->type_void, NULL);
 
     return true;
 }
