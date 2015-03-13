c3e47e69e433289eb837ecba6d8be047f556d3d4
Tom Harrison <tomh5908@gmail.com>
2015-03-14 09:10:55 +1000

    [master] tidying

--- src/dale/Form/Proc/Label/Label.cpp
+++ src/dale/Form/Proc/Label/Label.cpp
@@ -4,68 +4,52 @@
 #include "../../../Function/Function.h"
 #include "../../../llvm_Function.h"
 
+using namespace dale::ErrorInst::Generator;
+
 namespace dale
 {
 bool
-FormProcLabelParse(Units *units,
-           Function *fn,
-           llvm::BasicBlock *block,
-           Node *node,
-           bool get_address,
-           bool prefixed_with_core,
-           ParseResult *pr)
+FormProcLabelParse(Units *units, Function *fn, llvm::BasicBlock *block,
+                   Node *node, bool get_address, bool prefixed_with_core,
+                   ParseResult *pr)
 {
     Context *ctx = units->top()->ctx;
-
-    assert(node->list && "parseLabel must receive a list!");
 
     if (!ctx->er->assertArgNums("label", node, 1, 1)) {
         return false;
     }
 
     std::vector<Node *> *lst = node->list;
-    Node *lname = (*lst)[1];
+    Node *label_node = (*lst)[1];
 
-    if (!ctx->er->assertArgIsAtom("label", lname, "1")) {
+    if (!ctx->er->assertArgIsAtom("label", label_node, "1")) {
         return false;
     }
-    if (!ctx->er->assertAtomIsSymbol("label", lname, "1")) {
+    if (!ctx->er->assertAtomIsSymbol("label", label_node, "1")) {
         return false;
     }
-    Token *t = lname->token;
+    const char *label_name = label_node->token->str_value.c_str();
 
-    /* Create a block with this name. */
-
-    llvm::BasicBlock *new_block =
-        llvm::BasicBlock::Create(llvm::getGlobalContext(),
-                                 t->str_value.c_str(),
-                                 block->getParent());
-
-    /* If the existing block does not terminate, create a branch to
-     * this label. */
-
-    if (block->size() == 0
-            || !(block->back().isTerminator())) {
-        llvm::IRBuilder<> builder(block);
-        builder.CreateBr(new_block);
-    }
-
-    if (fn->getLabel(t->str_value.c_str())) {
-        Error *e = new Error(
-            ErrorInst::Generator::RedeclarationOfLabel,
-            node,
-            t->str_value.c_str()
-        );
+    if (fn->getLabel(label_name)) {
+        Error *e = new Error(RedeclarationOfLabel, node, label_name);
         ctx->er->addError(e);
         return false;
     }
 
+    llvm::BasicBlock *new_block =
+        llvm::BasicBlock::Create(llvm::getGlobalContext(), label_name,
+                                 block->getParent());
+
+    if (block->size() == 0 || !(block->back().isTerminator())) {
+        llvm::IRBuilder<> builder(block);
+        builder.CreateBr(new_block);
+    }
+
     int index = ++(ctx->ns()->lv_index);
-    Label *my_label = new Label(new_block, ctx->ns(), index);
-    fn->addLabel(t->str_value.c_str(), my_label);
-    
-    pr->set(new_block, ctx->tr->type_int,
-            ctx->nt->getNativeInt(0));
+    Label *label = new Label(new_block, ctx->ns(), index);
+    fn->addLabel(label_name, label);
+
+    pr->set(new_block, ctx->tr->type_int, ctx->nt->getNativeInt(0));
 
     return true;
 }
