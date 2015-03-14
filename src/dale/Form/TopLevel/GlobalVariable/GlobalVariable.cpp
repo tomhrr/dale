1b0821ea43be2322ebc1a0de26bb9a5d46cabfac
Tom Harrison <tomh5908@gmail.com>
2015-03-14 20:30:36 +1000

    [master] tidying

--- src/dale/Form/TopLevel/GlobalVariable/GlobalVariable.cpp
+++ src/dale/Form/TopLevel/GlobalVariable/GlobalVariable.cpp
@@ -14,6 +14,10 @@ using namespace dale::ErrorInst::Generator;
 namespace dale
 {
 llvm::Constant *
+parseLiteralElement(Units *units, Node *top, char *data, Type *type,
+                    int *size);
+
+llvm::Constant *
 apIntToConstant(llvm::APInt & ap_int)
 {
     llvm::ConstantInt *const_int =
@@ -22,287 +26,342 @@ apIntToConstant(llvm::APInt & ap_int)
 }
 
 llvm::Constant *
-parseLiteralInteger(int size, const char *value)
+apFloatToConstant(llvm::APFloat & ap_float)
 {
-    llvm::APInt ap_int(size, *value);
+    llvm::ConstantFP *const_float =
+        llvm::ConstantFP::get(llvm::getGlobalContext(), ap_float);
+    return llvm::cast<llvm::Constant>(const_float);
+}
+
+llvm::Constant *
+parseSmallLiteralInteger(int size, const char *data)
+{
+    llvm::APInt ap_int(size, *data);
     return apIntToConstant(ap_int);
 }
 
 llvm::Constant *
-parseLiteralElement(Units *units, Node *top, char *thing, Type *type,
-                    int *size)
+parseLiteralInteger(int size, const char *data)
+{
+    union mynum {
+        unsigned char udata[8];
+        uint64_t      nvalue;
+    } bling;
+    bling.nvalue = 0;
+
+    int i;
+    if (size == 128) {
+        uint64_t nvalues[2];
+        for (i = 0; i < 8; i++) {
+            bling.udata[i] = *(data + i);
+        }
+        nvalues[0] = bling.nvalue;
+        for (i = 8; i < 16; i++) {
+            bling.udata[i - 8] = *(data + i);
+        }
+        nvalues[1] = bling.nvalue;
+        llvm::APInt myint((unsigned) size, 2, nvalues);
+        return apIntToConstant(myint);
+    } else {
+        bling.nvalue = 0;
+        for (i = 0; i < (size / 8); i++) {
+            bling.udata[i] = *(data + i);
+        }
+        llvm::APInt myint(size, bling.nvalue);
+        return apIntToConstant(myint);
+    }
+}
+
+llvm::Constant *
+parseLiteralFloat(char *data)
+{
+    union float_hex {
+        unsigned char udata[4];
+        float         fvalue;
+    } bling;
+    for (int i = 3; i >= 0; i--) {
+        bling.udata[i] = data[i];
+    }
+    llvm::APFloat myfloat(bling.fvalue);
+    return apFloatToConstant(myfloat);
+}
+
+llvm::Constant *
+parseLiteralDouble(char *data)
+{
+    union double_hex {
+        unsigned char udata[8];
+        double        dvalue;
+    } bling;
+    for (int i = 7; i >= 0; i--) {
+        bling.udata[i] = data[i];
+    }
+    llvm::APFloat mydouble(bling.dvalue);
+    return apFloatToConstant(mydouble);
+}
+
+llvm::Constant *
+parseLiteralStruct(Units *units, Node *top, char *data, Type *type,
+                   int *size)
+{
+    Context *ctx = units->top()->ctx;
+
+    std::vector<llvm::Constant *> constants;
+
+    Struct *st = ctx->getStruct(type);
+    assert(st);
+
+    std::vector<Type *>::iterator begin =
+        st->member_types.begin();
+
+    int i = 0;
+    int last_el_size = -1;
+    int last_offset = -1;
+    int incr = 0;
+
+    while (begin != st->member_types.end()) {
+        Type *current = (*begin);
+        size_t el_size =
+            Operation::SizeofGet(units->top(), current);
+        size_t offset =
+            Operation::OffsetofGetByIndex(units->top(), type, i);
+        size_t padding = 0;
+        if (i != 0) {
+            padding = (offset - last_offset - last_el_size);
+        }
+        if (padding) {
+            Error *e = new Error(StructContainsPadding, top);
+            ctx->er->addError(e);
+        }
+        incr += padding;
+        char *addr = data;
+        addr += offset;
+        char aligned[256];
+        memcpy(aligned, addr, el_size);
+
+        llvm::Constant *el =
+            parseLiteralElement(units,
+                                top,
+                                (char*) aligned,
+                                current,
+                                size);
+        if (!el) {
+            return NULL;
+        }
+        constants.push_back(el);
+        last_offset  = offset - incr;
+        last_el_size = el_size;
+        ++i;
+        ++begin;
+    }
+
+    llvm::Type *llvm_type =
+        ctx->toLLVMType(type, NULL, false);
+    if (!llvm_type) {
+        return NULL;
+    }
+
+    llvm::StructType *llvm_st =
+        llvm::cast<llvm::StructType>(llvm_type);
+
+    llvm::Constant *init =
+        llvm::ConstantStruct::get(
+            llvm_st,
+            constants
+        );
+
+    return init;
+}
+
+llvm::Constant *
+parseLiteralString(Units *units, Node *top, char *data, Type *type,
+                   int *size)
 {
     Context *ctx = units->top()->ctx;
     NativeTypes *nt = ctx->nt;
     TypeRegister *tr = ctx->tr;
 
+    char *temp =
+        *(char**)
+        (((uintptr_t) data));
+    *size = strlen(temp) + 1;
+    llvm::Constant *myconststr =
+        llvm::cast<llvm::Constant>(
+#if D_LLVM_VERSION_MINOR < 2
+            llvm::ConstantArray::get(
+#else
+            llvm::ConstantDataArray::getString(
+#endif
+                                        llvm::getGlobalContext(),
+                                        temp,
+                                        true)
+        );
+
+    std::string varname2;
+    units->top()->getUnusedVarname(&varname2);
+
+    Type *archar =
+        tr->getArrayType(tr->type_char, *size);
+
+    if (units->top()->module->getGlobalVariable(llvm::StringRef(varname2.c_str()))) {
+        fprintf(stderr, "Internal error: "
+                "global variable already exists "
+                "in module ('%s').\n",
+                varname2.c_str());
+        abort();
+    }
+
+    llvm::GlobalVariable *svar2 =
+        llvm::cast<llvm::GlobalVariable>(
+            units->top()->module->getOrInsertGlobal(varname2.c_str(),
+                                    ctx->toLLVMType(archar, NULL, false))
+        );
+
+    svar2->setInitializer(myconststr);
+    svar2->setConstant(true);
+    svar2->setLinkage(ctx->toLLVMLinkage(Linkage::Intern));
+
+    llvm::Value *temps[2];
+    temps[0] = nt->getLLVMZero();
+    temps[1] = nt->getLLVMZero();
+
+    llvm::Constant *pce =
+        llvm::ConstantExpr::getGetElementPtr(
+            llvm::cast<llvm::Constant>(svar2),
+            temps,
+            2
+        );
+
+    return pce;
+}
+
+llvm::Constant *
+parseLiteralPointer(Units *units, Node *top, char *data, Type *type,
+                    int *size)
+{
+    Context *ctx = units->top()->ctx;
+
+    if (*data) {
+        uint64_t value = *(uint64_t*)data;
+        if (sizeof(char*) == 4) {
+            value <<= 32;
+            if (!value) {
+                goto a;
+            }
+        }
+        Error *e = new Error(NonNullPointerInGlobalStructDeclaration, top);
+        ctx->er->addError(e);
+    }
+a:
+    llvm::Type *llvm_type =
+        ctx->toLLVMType(type, NULL, false);
+    if (!llvm_type) {
+        return NULL;
+    }
+    llvm::Constant *pce =
+        llvm::ConstantPointerNull::get(
+            llvm::cast<llvm::PointerType>(llvm_type)
+        );
+    return pce;
+}
+
+llvm::Constant *
+parseLiteralArray(Units *units, Node *top, char *data, Type *type,
+                  int *size)
+{
+    Context *ctx = units->top()->ctx;
+
+    /* Take the portion devoted to whatever the element is,
+      * and re-call this function. */
+    size_t el_size =
+        Operation::SizeofGet(units->top(), type->array_type);
+    int i = 0;
+    int els = type->array_size;
+    std::vector<llvm::Constant *> constants;
+
+    char elmemm[256];
+    char *elmem = elmemm;
+
+    for (i = 0; i < els; i++) {
+        // Memset it to nodata.
+
+        memset(elmem, 0, 256);
+
+        // Offset data by the index, cast to a char pointer, and
+        // copy x elements into the new block.
+        char *mp = (char*) data;
+        mp += (i * el_size);
+        memcpy(elmem, mp, el_size);
+
+        // Re-call parseLiteralElement, push the new constant onto
+        // the vector.
+        llvm::Constant *mycon =
+            parseLiteralElement(units,
+                                top,
+                                elmem,
+                                type->array_type,
+                                size);
+
+        constants.push_back(mycon);
+    }
+
+    llvm::Constant *mine =
+        llvm::ConstantArray::get(
+            llvm::cast<llvm::ArrayType>(
+                ctx->toLLVMType(type, top, false, false)
+            ),
+            constants
+        );
+
+    return mine;
+}
+
+llvm::Constant *
+parseLiteralElement(Units *units, Node *top, char *data, Type *type,
+                    int *size)
+{
+    Context *ctx = units->top()->ctx;
+    NativeTypes *nt = ctx->nt;
+
     if (type->base_type == BaseType::Bool) {
-        return parseLiteralInteger(1, thing);
+        return parseSmallLiteralInteger(1, data);
     }
 
     if (type->base_type == BaseType::Char) {
-        return parseLiteralInteger(8, thing);
+        return parseSmallLiteralInteger(8, data);
     }
 
     if (type->isIntegerType()) {
-        union mynum {
-            unsigned char udata[8];
-            uint64_t      nvalue;
-        } bling;
-        bling.nvalue = 0;
-        int pr_size =
-            nt->internalSizeToRealSize(type->getIntegerSize());
-        int i;
-        if (pr_size == 128) {
-            uint64_t nvalues[2];
-            for (i = 0; i < 8; i++) {
-                bling.udata[i] = *(thing + i);
-            }
-            nvalues[0] = bling.nvalue;
-            for (i = 8; i < 16; i++) {
-                bling.udata[i - 8] = *(thing + i);
-            }
-            nvalues[1] = bling.nvalue;
-            llvm::APInt myint((unsigned) pr_size, 2, nvalues);
-            return apIntToConstant(myint);
-        } else {
-            bling.nvalue = 0;
-            for (i = 0; i < (pr_size / 8); i++) {
-                bling.udata[i] = *(thing + i);
-            }
-            llvm::APInt myint(pr_size,
-                              bling.nvalue);
-            return apIntToConstant(myint);
-        }
+        int size = nt->internalSizeToRealSize(type->getIntegerSize());
+        return parseLiteralInteger(size, data);
     }
 
     if (type->base_type == BaseType::Float) {
-        union float_hex {
-            unsigned char udata[4];
-            float         fvalue;
-        } bling;
-        for (int i = 3; i >= 0; i--) {
-            bling.udata[i] = thing[i];
-        }
-        llvm::APFloat myfloat(bling.fvalue);
-        llvm::ConstantFP *myconstfloat =
-            llvm::ConstantFP::get(llvm::getGlobalContext(),
-                                  myfloat);
-        return llvm::cast<llvm::Constant>(myconstfloat);
+        return parseLiteralFloat(data);
     }
 
     if (type->base_type == BaseType::Double) {
-        union double_hex {
-            unsigned char udata[8];
-            double        dvalue;
-        } bling;
-        for (int i = 7; i >= 0; i--) {
-            bling.udata[i] = thing[i];
-        }
-        llvm::APFloat mydouble(bling.dvalue);
-        llvm::ConstantFP *myconstdouble =
-            llvm::ConstantFP::get(llvm::getGlobalContext(),
-                                  mydouble);
-        return llvm::cast<llvm::Constant>(myconstdouble);
+        return parseLiteralDouble(data);
     }
 
     if (type->struct_name.size()) {
-        std::vector<llvm::Constant *> constants;
-
-        Struct *st = ctx->getStruct(type);
-        assert(st);
-
-        std::vector<Type *>::iterator begin =
-            st->member_types.begin();
-
-        int i = 0;
-        int last_el_size = -1;
-        int last_offset = -1;
-        int incr = 0;
-
-        while (begin != st->member_types.end()) {
-            Type *current = (*begin);
-            size_t el_size =
-                Operation::SizeofGet(units->top(), current);
-            size_t offset =
-                Operation::OffsetofGetByIndex(units->top(), type, i);
-            size_t padding = 0;
-            if (i != 0) {
-                padding = (offset - last_offset - last_el_size);
-            }
-            if (padding) {
-                Error *e = new Error(StructContainsPadding, top);
-                ctx->er->addError(e);
-            }
-            incr += padding;
-            char *addr = thing;
-            addr += offset;
-            char aligned[256];
-            memcpy(aligned, addr, el_size);
-
-            llvm::Constant *el =
-                parseLiteralElement(units,
-                                    top,
-                                    (char*) aligned,
-                                    current,
-                                    size);
-            if (!el) {
-                return NULL;
-            }
-            constants.push_back(el);
-            last_offset  = offset - incr;
-            last_el_size = el_size;
-            ++i;
-            ++begin;
-        }
-
-        llvm::Type *llvm_type =
-            ctx->toLLVMType(type, NULL, false);
-        if (!llvm_type) {
-            return NULL;
-        }
-
-        llvm::StructType *llvm_st =
-            llvm::cast<llvm::StructType>(llvm_type);
-
-        llvm::Constant *init =
-            llvm::ConstantStruct::get(
-                llvm_st,
-                constants
-            );
-
-        return init;
+        return parseLiteralStruct(units, top, data, type, size);
     }
 
     if (type->points_to && (type->points_to->base_type == BaseType::Char)) {
-        char *temp =
-            *(char**)
-            (((uintptr_t) thing));
-        *size = strlen(temp) + 1;
-        llvm::Constant *myconststr =
-            llvm::cast<llvm::Constant>(
-#if D_LLVM_VERSION_MINOR < 2
-                llvm::ConstantArray::get(
-#else
-                llvm::ConstantDataArray::getString(
-#endif
-                                         llvm::getGlobalContext(),
-                                         temp,
-                                         true)
-            );
-
-        std::string varname2;
-        units->top()->getUnusedVarname(&varname2);
-
-        Type *archar =
-            tr->getArrayType(tr->type_char, *size);
-
-        if (units->top()->module->getGlobalVariable(llvm::StringRef(varname2.c_str()))) {
-            fprintf(stderr, "Internal error: "
-                    "global variable already exists "
-                    "in module ('%s').\n",
-                    varname2.c_str());
-            abort();
-        }
-
-        llvm::GlobalVariable *svar2 =
-            llvm::cast<llvm::GlobalVariable>(
-                units->top()->module->getOrInsertGlobal(varname2.c_str(),
-                                       ctx->toLLVMType(archar, NULL, false))
-            );
-
-        svar2->setInitializer(myconststr);
-        svar2->setConstant(true);
-        svar2->setLinkage(ctx->toLLVMLinkage(Linkage::Intern));
-
-        llvm::Value *temps[2];
-        temps[0] = nt->getLLVMZero();
-        temps[1] = nt->getLLVMZero();
-
-        llvm::Constant *pce =
-            llvm::ConstantExpr::getGetElementPtr(
-                llvm::cast<llvm::Constant>(svar2),
-                temps,
-                2
-            );
-
-        return pce;
+        return parseLiteralString(units, top, data, type, size);
     }
 
     if (type->points_to) {
-        if (*thing) {
-            uint64_t value = *(uint64_t*)thing;
-            if (sizeof(char*) == 4) {
-                value <<= 32;
-                if (!value) {
-                    goto a;
-                }
-            }
-            Error *e = new Error(NonNullPointerInGlobalStructDeclaration, top);
-            ctx->er->addError(e);
-        }
-a:
-        llvm::Type *llvm_type =
-            ctx->toLLVMType(type, NULL, false);
-        if (!llvm_type) {
-            return NULL;
-        }
-        llvm::Constant *pce =
-            llvm::ConstantPointerNull::get(
-                llvm::cast<llvm::PointerType>(llvm_type)
-            );
-        return pce;
+        return parseLiteralPointer(units, top, data, type, size);
     }
 
     if (type->is_array) {
-        /* Take the portion devoted to whatever the element is,
-         * and re-call this function. */
-        size_t el_size =
-            Operation::SizeofGet(units->top(), type->array_type);
-        int i = 0;
-        int els = type->array_size;
-        std::vector<llvm::Constant *> constants;
-
-        char elmemm[256];
-        char *elmem = elmemm;
-
-        for (i = 0; i < els; i++) {
-            // Memset it to nothing.
-
-            memset(elmem, 0, 256);
-
-            // Offset thing by the index, cast to a char pointer, and
-            // copy x elements into the new block.
-            char *mp = (char*) thing;
-            mp += (i * el_size);
-            memcpy(elmem, mp, el_size);
-
-            // Re-call parseLiteralElement, push the new constant onto
-            // the vector.
-            llvm::Constant *mycon =
-                parseLiteralElement(units,
-                                    top,
-                                    elmem,
-                                    type->array_type,
-                                    size);
-
-            constants.push_back(mycon);
-        }
-
-        llvm::Constant *mine =
-            llvm::ConstantArray::get(
-                llvm::cast<llvm::ArrayType>(
-                    ctx->toLLVMType(type, top, false, false)
-                ),
-                constants
-            );
-
-        return mine;
+        return parseLiteralArray(units, top, data, type, size);
     }
 
     std::string type_str;
     type->toString(&type_str);
-
     Error *e = new Error(CannotParseLiteral, top, type_str.c_str());
     ctx->er->addError(e);
 
