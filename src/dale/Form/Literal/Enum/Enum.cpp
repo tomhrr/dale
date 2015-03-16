ed7136a1d0d49c69ff887c9b442cdfa01f85cb69
Tom Harrison <tomh5908@gmail.com>
2015-03-16 14:46:01 +1000

    [master] tidying

--- src/dale/Form/Literal/Enum/Enum.cpp
+++ src/dale/Form/Literal/Enum/Enum.cpp
@@ -2,72 +2,60 @@
 #include "../../../Linkage/Linkage.h"
 #include "../../../Error/Error.h"
 
+using namespace dale::ErrorInst::Generator;
+
 namespace dale {
 bool 
-FormLiteralEnumParse(Units *units,
-      llvm::BasicBlock *block,
-      Node *n,
-      Enum *myenum,
-      Type *myenumtype,
-      Struct *myenumstructtype,
-      bool get_address,
-      ParseResult *pr)
+FormLiteralEnumParse(Units *units, llvm::BasicBlock *block, Node *node,
+                     Enum *enum_obj, Type *enum_type,
+                     Struct *enum_st, bool get_address,
+                     ParseResult *pr)
 {
     Context *ctx = units->top()->ctx;
 
-    if (!n->is_token) {
-        Error *e = new Error(
-            ErrorInst::Generator::UnexpectedElement, n,
-            "atom", "enum literal", "list"
-        );
+    if (!node->is_token) {
+        Error *e = new Error(UnexpectedElement, node,
+                             "atom", "enum literal", "list");
         ctx->er->addError(e);
         return false;
     }
 
-    if (!myenum->existsMember(n->token->str_value.c_str())) {
-        Error *e = new Error(
-            ErrorInst::Generator::EnumValueDoesNotExist,
-            n,
-            n->token->str_value.c_str()
-        );
+    const char *member_name = node->token->str_value.c_str();
+    if (!enum_obj->existsMember(member_name)) {
+        Error *e = new Error(EnumValueDoesNotExist, node, member_name);
         ctx->er->addError(e);
         return false;
     }
-    int num = myenum->memberToIndex(n->token->str_value.c_str());
+    int member_index = enum_obj->memberToIndex(member_name);
 
     llvm::IRBuilder<> builder(block);
-
-    llvm::Value *sp = llvm::cast<llvm::Value>(
-                          builder.CreateAlloca(myenumstructtype->type)
-                      );
+    llvm::Value *storage =
+        llvm::cast<llvm::Value>(builder.CreateAlloca(enum_st->type));
 
     std::vector<llvm::Value *> two_zero_indices;
     STL::push_back2(&two_zero_indices,
-                        ctx->nt->getLLVMZero(), ctx->nt->getLLVMZero());
-    llvm::Value *res =
-        builder.CreateGEP(sp,
+                    ctx->nt->getLLVMZero(), ctx->nt->getLLVMZero());
+
+    llvm::Value *storage_ptr =
+        builder.CreateGEP(storage,
                           llvm::ArrayRef<llvm::Value*>(two_zero_indices));
 
     llvm::Type *llvm_type =
-        ctx->toLLVMType(myenumstructtype->member_types.at(0),
-                       NULL, false);
+        ctx->toLLVMType(enum_st->member_types.at(0), NULL, false);
     if (!llvm_type) {
         return false;
     }
 
-    builder.CreateStore(llvm::ConstantInt::get(
-                            llvm_type, num),
-                        res);
+    builder.CreateStore(llvm::ConstantInt::get(llvm_type, member_index),
+                        storage_ptr);
 
     if (get_address) {
-        pr->set(block, ctx->tr->getPointerType(myenumtype), sp);
-        return true;
+        pr->set(block, ctx->tr->getPointerType(enum_type), storage);
     } else {
-        llvm::Value *final_value =
-            builder.CreateLoad(sp);
-
-        pr->set(block, myenumtype, final_value);
-        return true;
+        llvm::Value *final_value = builder.CreateLoad(storage);
+        pr->set(block, enum_type, final_value);
     }
+
+    return true;
 }
 }
--- src/dale/Form/Literal/Enum/Enum.h
+++ src/dale/Form/Literal/Enum/Enum.h
@@ -7,15 +7,20 @@
 #include "../../../Type/Type.h"
 
 namespace dale {
-bool
-FormLiteralEnumParse(Units *units,
-      llvm::BasicBlock *block,
-      Node *n,
-      Enum *myenum,
-      Type *myenumtype,
-      Struct *myenumstructtype,
-      bool get_address,
-      ParseResult *pr);
+/*! Parse an enum literal.
+ *  @param units The units context.
+ *  @param block The current block.
+ *  @param node The node containing the enum.
+ *  @param enum_obj The literal's expected enum definition.
+ *  @param enum_type The literal's expected enum type.
+ *  @param enum_st The literal's expected enum struct.
+ *  @param get_address Whether to return the address of the enum.
+ *  @param pr The parse result for the enum.
+ */
+bool FormLiteralEnumParse(Units *units, llvm::BasicBlock *block,
+                          Node *node, Enum *enum_obj, Type *enum_type,
+                          Struct *enum_st, bool get_address,
+                          ParseResult *pr);
 }
 
 #endif
