d08da843f837f50b317d670aa30057717d505c5f
Tom Harrison <tomh5908@gmail.com>
2016-09-16 13:07:06 +1000

    [master] attempt to make it work for llvm 3.4

--- src/dale/Operation/Sizeof/Sizeof.cpp
+++ src/dale/Operation/Sizeof/Sizeof.cpp
@@ -12,7 +12,12 @@ SizeofGet(Unit *unit,
 {
     Context *ctx = unit->ctx;
     llvm::Type *llvm_type = ctx->toLLVMType(type, NULL, false);
+#if D_LLVM_VERSION_MINOR <= 4
+    llvm::DataLayout data_layout(unit->module->getDataLayout());
+    return data_layout.getTypeSizeInBits(llvm_type) / 8;
+#else
     return unit->module->getDataLayout()->getTypeSizeInBits(llvm_type) / 8;
+#endif
 }
 }
 }
