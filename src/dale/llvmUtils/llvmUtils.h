e01f2b69dd1c4f0843c4df80d825e43f906392c7
Tom Harrison <tomh5908@gmail.com>
2018-04-25 21:44:30 +1000

    [master] missing header

--- src/dale/llvmUtils/llvmUtils.h
+++ src/dale/llvmUtils/llvmUtils.h
@@ -26,6 +26,7 @@
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Support/raw_ostream.h"
 
 #define _unused(x) ((void)x)
 
